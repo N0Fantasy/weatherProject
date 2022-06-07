f8a9cd87a84b44d2e6416a915be12943095802c3
N0Fantasy <new-e-box@mail.ru>
2022-06-07 12:18:26 +0300

    first

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,10 +1,15 @@
-import { Input } from './Input';
 import './App.css';
+
+import { Input } from './Input';
+import { Card } from './Card';
 
 function App() {
   return (
     <div className="Main">
       <Input />
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
